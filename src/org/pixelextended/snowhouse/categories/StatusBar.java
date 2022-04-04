77095d6d6ffd45c1eb8f52f14b80e04191556a1e
ralf979 <ralf979@gmail.com>
2022-04-04 16:35:10 +0000

    SnowHouse: Update Qs Brightness slider settings
    adapt for new implementation

--- src/org/pixelextended/snowhouse/categories/StatusBar.java
+++ src/org/pixelextended/snowhouse/categories/StatusBar.java
@@ -23,6 +23,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import android.os.UserHandle;
 
@@ -35,7 +36,14 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     private static final String TAG = "StatusBar";
 
+    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
     private ListPreference mQuickPulldown;
+    private ListPreference mBrightnessSliderPosition;
+    private ListPreference mShowBrightnessSlider;
+    private SwitchPreference mShowAutoBrightness;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,7 +51,26 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.status_bar);
 
-        ContentResolver resolver = getActivity().getContentResolver();
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean showSlider = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
+
+        mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+        mBrightnessSliderPosition.setEnabled(showSlider);
+
+        mShowAutoBrightness = findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
+        boolean automaticAvailable = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (automaticAvailable) {
+            mShowAutoBrightness.setEnabled(showSlider);
+        } else {
+            prefSet.removePreference(mShowAutoBrightness);
+        }
 
         int qpmode = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
@@ -70,8 +97,13 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        final String key = preference.getKey();
-         if (preference == mQuickPulldown) {
+        if (preference == mShowBrightnessSlider) {
+            int value = Integer.parseInt((String) newValue);
+            mBrightnessSliderPosition.setEnabled(value > 0);
+            if (mShowAutoBrightness != null)
+                mShowAutoBrightness.setEnabled(value > 0);
+            return true;
+        } else if (preference == mQuickPulldown) {
             int value = Integer.parseInt((String) newValue);
             Settings.System.putIntForUser(resolver,
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value,
@@ -80,7 +112,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
             mQuickPulldown.setSummary(
                     mQuickPulldown.getEntries()[index]);
             return true;
-         }
+        }
         return false;
     }
 
